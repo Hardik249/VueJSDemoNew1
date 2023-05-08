c9392ddee2878171ffb41ce6d2cc85116ef2f877
Hardik <hardik.karavyasolutions@gamil.com>
2023-05-08 19:34:42 +0530

    add to cart for the case scenario second for the logged in user started today

--- src/components/Cart/modalCart.vue
+++ src/components/Cart/modalCart.vue
@@ -31,8 +31,28 @@
                 <img src="@/assets/empty.svg" alt="Empty cart">
                 <h4 style="color: var(--secondary);"> Your cart is empty </h4>
                 <small> Add somthing to make me happy for real :-) </small>
-            </div>
-            <modalCard v-for="item_in_cart in store_cart.getItems" :image="item_in_cart.item.thumbnail"
+            </div>{{cart_products}}
+            <modalCard v-for="item_in_cart in cart_products.data" :image="item_in_cart.thumbnail"
+                :quantity="item_in_cart.number" :computedPrice="item_in_cart.number * item_in_cart.price">
+                {{ item_in_cart.title }}
+                <template v-slot:reduceQuantity>
+                    <button class="handleQuantity" @click="reduceQuantity(item_in_cart)"> - </button>
+                </template>
+                <template v-slot:addQuantity>
+                    <button class="handleQuantity" @click="addQuantity(item_in_cart)"> + </button>
+                </template>
+                <template v-slot:btnDelete>
+                    <svg @click="deleteItem(item_in_cart)" class="trashIcon" width="24" height="24" viewBox="0 0 24 24"
+                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
+                            fill="currentColor" />
+                        <path d="M9 9H11V17H9V9Z" fill="currentColor" />
+                        <path d="M13 9H15V17H13V9Z" fill="currentColor" />
+                    </svg>
+                </template>
+            </modalCard>
+            <!-- <modalCard v-for="item_in_cart in store_cart.getItems" :image="item_in_cart.item.thumbnail"
                 :quantity="item_in_cart.number" :computedPrice="item_in_cart.number * item_in_cart.item.price">
                 {{ item_in_cart.item.title }}
                 <template v-slot:reduceQuantity>
@@ -51,7 +71,7 @@
                         <path d="M13 9H15V17H13V9Z" fill="currentColor" />
                     </svg>
                 </template>
-            </modalCard>
+            </modalCard> -->
             <div class="total" v-if="store_cart.getItems.length !== 0">
                 <h5> Total <span class="amount"> {{ total }}€ </span> </h5>
             </div>
@@ -76,11 +96,20 @@
 </template>
 
 <script setup>
-import { ref, inject, watchEffect, watch } from 'vue';
+import { onMounted, ref, inject, watchEffect, watch } from 'vue';
 import modalCard from './modalCard.vue';
 import { useCart } from '@/store/cart.store.js'
 import { element_index_in_array } from '@/services/utils/utils'
 
+let limit = ref(0);
+// let total = ref(0);
+let totalProducts = ref(0);
+let length = ref(0);
+let items = ref([]);
+let columns = ref([]);
+let totalProductsLimit = ref(0);
+
+let cart_products = ref([]);
 const store_cart = useCart()
 const isOpen = inject('dataModalCart')
 
@@ -103,6 +132,39 @@ const check_promo_code = () => {
     // Checker' l'exactiture du code promo 3 secondes eprès l'entree ... en attendant, faire apparaitre un loader.... 
     console.log(promo_value)
 }
+
+onMounted(async () => {
+    await axios
+        .get('http://localhost:3000/api/carts/viewcarts')
+        .then(response => {
+            console.log('vcp', response.data.data)
+            // products.value = response.data.products
+            cart_products.value = response.data
+            limit.value = 6;
+            items.value = response;
+            columns.value = response.data.data ? Object.keys(response.data.data[0]) : '';
+            totalProducts.value = response.data;
+            totalProductsLimit.value = response.data.limit
+            total.value = response.data.total;
+            console.log('total', total)
+            // length = response.data.value.length;
+            // max_price.value = check_max_price(response.data.products._rawValue)
+            // max_price.value = check_max_price(products)
+        });
+        // if (store_categories.getAllCategories.length === 0) {
+        //     await axios
+        //         .get('https://dummyjson.com/products/categories')
+        //         .then(response => {
+        //             store_categories.add_all_categories(JSON.parse(JSON.stringify(response.data)))
+        //             categories.value = store_categories.getAllCategories
+        //         })
+        // }
+        // else {
+        //     categories.value = store_categories.getAllCategories
+        // }
+})
+
+
 watchEffect(() => {
     total.value = store_cart.total_amount().toFixed(2)
     console.log(isOpen + "zfzfzefzefez")
